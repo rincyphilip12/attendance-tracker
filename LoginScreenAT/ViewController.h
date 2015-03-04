8ed1054397e961c0caefe156c48e26d2ecf923ff
RiN <rin@aryalakshmi.local>
2015-03-04 11:45:21 +0530

    Initial Commit

---
+++ LoginScreenAT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginScreenAT
+//
+//  Created by RiN on 04/03/15.
+//  Copyright (c) 2015 RiN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
